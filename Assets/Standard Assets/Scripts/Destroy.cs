9d494c0ebaf4535aa73d929dfce973a9d04f9d3e
angeltypes <157560431+angeltypes@users.noreply.github.com>
2026-02-05 07:09:58 -0500

    added sfx

--- Assets/Standard Assets/Scripts/Destroy.cs
+++ Assets/Standard Assets/Scripts/Destroy.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class DestroyOnClick : MonoBehaviour
 {
     [SerializeField] GameObject BloodVFX;
-    [SerializeField] GameObject MeatVFX;
+    public AudioClip clip;
     void OnMouseDown()
     {
         GameObject explosion1 = Instantiate(BloodVFX, transform.position, transform.rotation);
-
+        AudioSource.PlayClipAtPoint(clip, new Vector3(-9, 2, -40));
         // Destroy the GameObject this script is attached to
         Destroy(gameObject);
 
